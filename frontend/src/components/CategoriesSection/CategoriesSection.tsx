e63995a493e9a6f16c2b607b86c22b74954bf991
Troth99 <trothlol123456@gmail.com>
2025-11-09 06:20:18 +0200

    Added css styles to the component

--- frontend/src/components/CategoriesSection/CategoriesSection.tsx
+++ frontend/src/components/CategoriesSection/CategoriesSection.tsx
@@ -1,38 +1,41 @@
 import { useEffect, useState } from "react";
 import { Category, getAllCategories } from "../../services/categoryService";
+import "./CategoriesSection.css";
 
+export default function CategoriesSection() {
+  const [categories, setCategories] = useState<Category[]>([]);
+  const [showAll, setShowAll] = useState(false);
 
-
-export default function CategoriesSection()  {
-  const [categories, setCategories] = useState<Category[]>([])
-const [showAll, setShowAll] = useState(false);
-
-
- useEffect(() => {
+  useEffect(() => {
     async function fetchCategories() {
-        try {
-            const data = await getAllCategories()
-            setCategories(data)
-        } catch (error) {
-            console.error('Failed to load categories', error)
-        }
+      try {
+        const data = await getAllCategories();
+        setCategories(data);
+      } catch (error) {
+        console.error("Failed to load categories", error);
+      }
     }
     fetchCategories();
- }, [])
+  }, []);
 
-  const visibleCategories = showAll ? categories : categories.slice(0, 8)
+  const visibleCategories = showAll ? categories : categories.slice(0, 8);
   return (
-      <div className="categories-section">
-      <h2>Job Categories</h2>
+   <div className="categories-section">
       <div className="categories-grid">
         {visibleCategories.map((cat) => (
-            <div key={cat._id}>{cat.name}</div>
+          <div key={cat._id}>
+            <div className="card-body">
+              <div className="one-line">
+                <span>{cat.name}</span>
+              </div>
+            </div>
+          </div>
         ))}
       </div>
 
       {categories.length > 8 && (
         <button onClick={() => setShowAll(!showAll)} className="show-all-btn">
-          {showAll ? "Show Less" : "Show All Categories"}
+          {showAll ? "Show Less" : "View All Categories"}
         </button>
       )}
     </div>
