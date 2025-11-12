059f79442b6ffff1f44f0ae2ab245c201912c069
Troth99 <trothlol123456@gmail.com>
2025-11-12 05:55:28 +0200

    fixed dynamic error handler on inputs

--- frontend/src/components/auth/Register/Register.tsx
+++ frontend/src/components/auth/Register/Register.tsx
@@ -8,7 +8,6 @@ import {
   registerUser,
 } from "../../../services/auth/authService";
 
-
 export default function RegisterComponent() {
   const [serverErrors, setServerErrors] = useState<FieldErrors>({});
   const [loading, setLoading] = useState(false);
@@ -25,26 +24,23 @@ export default function RegisterComponent() {
     location: "",
   });
 
-
   const registerHandler = async (e: React.FormEvent) => {
     e.preventDefault();
     setLoading(true);
     setServerErrors({});
 
-      try {
-    const res = await registerUser(formData);
+    try {
+      const res = await registerUser(formData);
 
-    if (res.token) {
-      localStorage.setItem("token", res.token);
-      navigate("/");
+      if (res.token) {
+        localStorage.setItem("token", res.token);
+        navigate("/");
+      }
+    } catch (err: any) {
+      setServerErrors(err.fieldErrors || {});
+    } finally {
+      setLoading(false);
     }
-  } catch (err: any) {
-     
-    setServerErrors(err.fieldErrors || {})
-
-  } finally {
-    setLoading(false);
-  }
   };
   return (
     <div className="register-wrapper">
@@ -57,53 +53,91 @@ export default function RegisterComponent() {
           <form id="registerForm" onSubmit={registerHandler}>
             <div className="input-wrap">
               <i className="fa-solid fa-user"></i>
-              <input type="text" placeholder="First name"
-              value={formData.firstName}
-              onChange={(e) => setFormData({ ...formData, firstName: e.target.value})}
+              <input
+                type="text"
+                placeholder="First name"
+                value={formData.firstName}
+                onChange={(e) => {
+                  setFormData({ ...formData, firstName: e.target.value });
+
+                  setServerErrors((prev) => ({ ...prev, firstName: "" }));
+                }}
               />
               <div className="error-message">{serverErrors.firstName}</div>
             </div>
             <div className="input-wrap">
               <i className="fa-solid fa-user"></i>
-              <input type="text" placeholder="Last name" 
-              value={formData.lastName}
-              onChange={(e) => setFormData({...formData, lastName: e.target.value})}
+              <input
+                type="text"
+                placeholder="Last name"
+                value={formData.lastName}
+                    onChange={(e) => {
+                  setFormData({ ...formData, lastName: e.target.value });
+
+                  setServerErrors((prev) => ({ ...prev, lastName: "" }));
+                }}
               />
               <div className="error-message">{serverErrors.lastName}</div>
             </div>
             <div className="input-wrap">
               <i className="fa-solid fa-envelope"></i>
-              <input type="email" placeholder="Email address" 
-               value={formData.email}
-              onChange={(e) => setFormData({...formData, email: e.target.value})}
+              <input
+                type="email"
+                placeholder="Email address"
+                value={formData.email}
+                   onChange={(e) => {
+                  setFormData({ ...formData, email: e.target.value });
+
+                  setServerErrors((prev) => ({ ...prev, email: "" }));
+                }}
               />
-             <div className="error-message">{serverErrors.email}</div>
+              <div className="error-message">{serverErrors.email}</div>
             </div>
             <div className="input-wrap">
               <i className="fa-solid fa-phone"></i>
-              <input type="tel" placeholder="Phone Number" 
-               value={formData.phoneNumber}
-              onChange={(e) => setFormData({...formData, phoneNumber: e.target.value})}
+              <input
+                type="tel"
+                placeholder="Phone Number"
+                value={formData.phoneNumber}
+                  onChange={(e) => {
+                  setFormData({ ...formData, phoneNumber: e.target.value });
+
+                  setServerErrors((prev) => ({ ...prev, phoneNumber: "" }));
+                }}
               />
-                <div className="error-message">{serverErrors.phoneNumber}</div>
+              <div className="error-message">{serverErrors.phoneNumber}</div>
             </div>
             <div className="input-wrap">
               <i className="fa-solid fa-location-dot"></i>
-              <input type="text" placeholder="City / Location" 
+              <input
+                type="text"
+                placeholder="City / Location"
                 value={formData.location}
-              onChange={(e) => setFormData({...formData, location: e.target.value})}
+                  onChange={(e) => {
+                  setFormData({ ...formData, location: e.target.value });
+
+                  setServerErrors((prev) => ({ ...prev, location: "" }));
+                }}
               />
               <div className="error-message">{serverErrors.location}</div>
             </div>
             <div className="input-wrap">
               <i className="fa-solid fa-lock"></i>
-              <input type="password" placeholder="Password" id="pwd"
+              <input
+                type="password"
+                placeholder="Password"
+                id="pwd"
                 value={formData.password}
-              onChange={(e) => setFormData({...formData, password: e.target.value})} />
+                  onChange={(e) => {
+                  setFormData({ ...formData, password: e.target.value });
+
+                  setServerErrors((prev) => ({ ...prev, password: "" }));
+                }}
+              />
               <button type="button" className="show-hide-btn" id="togglePwd">
                 👁
               </button>
-               <div className="error-message">{serverErrors.password}</div>
+              <div className="error-message">{serverErrors.password}</div>
             </div>
             <div className="input-wrap">
               <i className="fa-solid fa-lock"></i>
@@ -111,9 +145,10 @@ export default function RegisterComponent() {
                 type="password"
                 placeholder="Confirm Password"
                 id="confirmPwd"
-                
               />
-              <div className="error-message">{serverErrors.confirmPassword}</div>
+              <div className="error-message">
+                {serverErrors.confirmPassword}
+              </div>
               <button
                 type="button"
                 className="show-hide-btn"
@@ -144,3 +179,5 @@ export default function RegisterComponent() {
     </div>
   );
 }
+
+//TO DO password validation matching and eye on the form
