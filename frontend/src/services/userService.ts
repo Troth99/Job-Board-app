458ccf94f45bd978166f5e35f63d1ed54f2d0a56
Troth99 <trothlol123456@gmail.com>
2025-11-15 16:47:14 +0200

    Added imageBB api for uploading files

--- frontend/src/services/userService.ts
+++ frontend/src/services/userService.ts
@@ -25,3 +25,38 @@ export async function getUserProfile() {
     throw new Error(error?.message || "Failed to fetch user data.");
   }
 }
+
+
+export async function uploadUserProfileImage(file: File): Promise<string> {
+    const apiKey = '844b750f8696c887633d12684dff203e'
+    const formData = new FormData();
+
+    formData.append('image', file)
+
+      const response = await fetch(`https://api.imgbb.com/1/upload?key=${apiKey}`, {
+    method: "POST",
+    body: formData,
+  });
+
+  const data = await response.json()
+
+    if (!data.success) throw new Error("Upload failed");
+  console.log(data.data.url)
+      return data.data.url; 
+
+}
+
+export async function updateUserProfile(data: { avatar?: string; [key: string]: any }) {
+  const token = getAuthToken();
+  if (!token) throw new Error("User not authenticated");
+
+    return sendRequest(
+      `${API_BASE}/users/me`,
+      "PUT",
+      data,
+      {
+        Authorization: `Bearer ${token}`,
+      }
+    );
+
+}
