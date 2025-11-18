2eeb03e449f0ae452fdd4baabede8571581c9ae5
Troth99 <trothlol123456@gmail.com>
2025-11-19 00:14:51 +0200

    Added spinner to edit from

--- frontend/src/components/EditProfile/EditProfile.tsx
+++ frontend/src/components/EditProfile/EditProfile.tsx
@@ -24,7 +24,7 @@ export interface ProfileData {
 const initialProfileData: ProfileData = {
   firstName: "",
   lastName: "",
-  email: "",  
+  email: "",
   phoneNumber: "",
   location: "",
   avatar: "",
@@ -33,11 +33,11 @@ export default function EditProfile() {
   const [avatarInfo, setAvatarInfo] = useState({
     avatar: "",
   });
-  const [profileData, setProfileData] = useState<ProfileData>(initialProfileData);
+  const [profileData, setProfileData] =
+    useState<ProfileData>(initialProfileData);
   const [loading, setLoading] = useState<boolean>(true);
   const [buttonLoading, setButtonLoading] = useState<boolean>(false);
   const [errors, setErrors] = useState<Partial<registerFormType>>({});
-
 
   const { validateForm } = useValidation();
   const navigate = useNavigate();
@@ -56,23 +56,26 @@ export default function EditProfile() {
     fetchProfile();
   }, []);
 
-const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-  const { name, value } = e.target;
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
 
-  const trimmedValue = value.trim();
+    const trimmedValue = value.trim();
 
-  setProfileData((prev) => ({ ...prev as ProfileData, [name]: trimmedValue }));
-  setErrors((prev) => ({ ...prev, [name]: "" }));
-};
+    setProfileData((prev) => ({
+      ...(prev as ProfileData),
+      [name]: trimmedValue,
+    }));
+    setErrors((prev) => ({ ...prev, [name]: "" }));
+  };
 
   const handleDeleteProfileImage = async (e: React.FormEvent) => {
     e.preventDefault();
     try {
       const response = await deleteUserProfileImage();
-      if(response.message === 'Profile image deleted successfully'){
-          setAvatarInfo(prev => ({ ...prev, avatar: "" }));
-          showSuccess('Profile image deleted successfully!')
-          navigate('/profile')
+      if (response.message === "Profile image deleted successfully") {
+        setAvatarInfo((prev) => ({ ...prev, avatar: "" }));
+        showSuccess("Profile image deleted successfully!");
+        navigate("/profile");
       }
     } catch (error) {
       console.error("Error deleting profile image:", error);
@@ -103,103 +106,107 @@ const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
       setButtonLoading(false);
     }
   };
-// to Refractor to controlled form
+
   return (
     <div className="profile-body" style={{ position: "relative" }}>
       {loading && <Spinner overlay={true} />}
-      <div className="profile-container">
-        <div className="profile-header">
-          <h1>Edit Profile</h1>
-        </div>
 
-        <form onSubmit={editSubmitHandler}>
-          <div className="profile-details">
-            <div>
-              <strong>First name:</strong>
-              <input
-                type="text"
-                value={profileData?.firstName}
-                name="firstName"
-                onChange={handleInputChange}
-              />
+      {!loading && (
+        <div className="profile-container">
+          <div className="profile-header">
+            <h1>Edit Profile</h1>
+          </div>
+
+          <form onSubmit={editSubmitHandler}>
+            <div className="profile-details">
+              <div>
+                <strong>First name:</strong>
+                <input
+                  type="text"
+                  value={profileData?.firstName}
+                  name="firstName"
+                  onChange={handleInputChange}
+                />
                 <div className="error-message">{errors.firstName}</div>
+              </div>
+              <div>
+                <strong>Last name:</strong>
+                <input
+                  type="text"
+                  value={profileData?.lastName}
+                  name="lastName"
+                  onChange={handleInputChange}
+                />
+                <div className="error-message">{errors.lastName}</div>
+              </div>
+              <div>
+                <strong>Phone:</strong>
+                <input
+                  type="text"
+                  value={profileData?.phoneNumber}
+                  name="phoneNumber"
+                  onChange={handleInputChange}
+                />
+                <div className="error-message">{errors.phoneNumber}</div>
+              </div>
+              <div>
+                <strong>Location:</strong>
+                <input
+                  type="text"
+                  value={profileData?.location}
+                  name="location"
+                  onChange={handleInputChange}
+                />
+                <div className="error-message">{errors.location}</div>
+              </div>
+
+              <div className="edit-profile-button-container">
+                <button
+                  className="edit-profile-button"
+                  type="submit"
+                  disabled={buttonLoading}
+                >
+                  {buttonLoading ? "Saving..." : "Save Changes"}
+                </button>
+              </div>
             </div>
-            <div>
-              <strong>Last name:</strong>
-              <input
-                type="text"
-                value={profileData?.lastName}
-                name="lastName"
-                onChange={handleInputChange}
-              />
-              <div className="error-message">{errors.lastName}</div>
-            </div>
-            <div>
-              <strong>Phone:</strong>
-              <input
-                type="text"
-                value={profileData?.phoneNumber}
-                name="phoneNumber"
-                onChange={handleInputChange}
-                
-              />
-              <div className="error-message">{errors.phoneNumber}</div>
-            </div>
-            <div>
-              <strong>Location:</strong>
-              <input
-                type="text"
-                value={profileData?.location}
-                name="location"
-                onChange={handleInputChange}
-              />
-              <div className="error-message">{errors.location}</div>
+            <div className="button-container">
+              <div className="delete-image-container">
+                <button
+                  className="delete-image-button"
+                  onClick={handleDeleteProfileImage}
+                >
+                  Delete Profile Image
+                </button>
+              </div>
+              <div className="check-profile-logs-container">
+                <button className="check-profile-logs-button">View logs</button>
+              </div>
+              <div className="change-password-container">
+                <button className="change-password-button">
+                  Change Password
+                </button>
+              </div>
+
+              <div className="delete-profile-container">
+                <button className="delete-profile-button">
+                  Delete Profile
+                </button>
+              </div>
             </div>
 
-            <div className="edit-profile-button-container">
-              <button
-                className="edit-profile-button"
-                type="submit"
-                disabled={buttonLoading}
-              >
-                {buttonLoading ? "Saving..." : "Save Changes"}
-              </button>
-            </div>
-          </div>
-          <div className="button-container">
-            <div className="delete-image-container">
-              <button
-                className="delete-image-button"
-                onClick={handleDeleteProfileImage}
-              >
-                Delete Profile Image
-              </button>
-            </div>
-            <div className="check-profile-logs-container">
-              <button className="check-profile-logs-button">View logs</button>
-            </div>
-            <div className="change-password-container">
-              <button className="change-password-button">
-                Change Password
-              </button>
+            <div className="role-change">
+              <h3>Change Role</h3>
+              <p>Select a new role for your profile.</p>
             </div>
 
-            <div className="delete-profile-container">
-              <button className="delete-profile-button">Delete Profile</button>
+            <div className="company-registration">
+              <h3>Company Registration</h3>
+              <p>If you have registered a new company, you can add it here.</p>
             </div>
-          </div>
-
-          <div className="role-change">
-            <h3>Change Role</h3>
-            <p>Select a new role for your profile.</p>
-          </div>
-
-          <div className="company-registration">
-            <h3>Company Registration</h3>
-            <p>If you have registered a new company, you can add it here.</p>
-          </div>
-        </form>
-      </div>
+          </form>
+        </div>
+      )}
     </div>
   );
 }
