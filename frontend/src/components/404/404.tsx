85e904b45c81476bc2cc6f18b94a079e64cc1f79
Troth99 <trothlol123456@gmail.com>
2025-11-22 06:07:05 +0200

    Refractored 404 not found and fixed css bug with memberDashboard

--- frontend/src/components/404/404.tsx
+++ frontend/src/components/404/404.tsx
@@ -3,22 +3,17 @@ import "./404.css"
 
 
 export function PageNotFound() {
-
+//to refractor css and 404 not found
     return (
- <div className="notfound-wrapper">
-      <div className="notfound-box">
-        <h1>404</h1>
-        <p>Oops! No job listings found here...</p>
-
-        <div className="job-animation">
-          <div className="job-card-404">Frontend</div>
-          <div className="job-card-404">Backend</div>
-          <div className="job-card-404">Designer</div>
-          <div className="job-card-404">DevOps</div>
-          <div className="job-card-404">QA</div>
-        </div>
-
-        <a href="/">Return to Job Board</a>
+    <div className="pnf-wrapper">
+      <div className="pnf-container">
+        <h1 className="pnf-title">
+          <span className="pnf-digit">4</span>
+          <span className="pnf-digit">0</span>
+          <span className="pnf-digit">4</span>
+        </h1>
+        <p className="pnf-subtitle">Oops! Page not found.</p>
+        <Link className="pnf-button" to="/">Return to Job Board</Link>
       </div>
     </div>
     )
