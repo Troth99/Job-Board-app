936f9189dfc84cbfa23267f97b2f790b4f40c761
Troth99 <trothlol123456@gmail.com>
2025-11-24 18:58:57 +0200

    Fixed job details and edit guard
    1. If the user navigates from the url to a job that is not from the current company it navigates to home page and toast error.

--- frontend/src/utils/RouteGuards/jobEditRouteGuard.tsx
+++ frontend/src/utils/RouteGuards/jobEditRouteGuard.tsx
@@ -4,6 +4,8 @@ import { toast } from "react-toastify";
 import { getAuthToken, getUserFromLocalStorage } from "../../services/auth/authService";
 import { getCompanyById, getUserRole } from "../../services/companyService";
 import Spinner from "../../components/Spinner/Spinner";
+import { Job } from "../../components/Jobs/CreateJob/CreateJob";
+import { getJobById } from "../../services/jobService";
 
 
 export  function JobEditRouteGuard({ children }: {children: React.ReactNode}) {
@@ -12,8 +14,10 @@ export  function JobEditRouteGuard({ children }: {children: React.ReactNode}) {
   const { companyId, jobId } = useParams<{ companyId: string; jobId: string }>()
   const user = getUserFromLocalStorage();
    const [loading, setLoading] = useState(true)
+const [currentJob, setCurrentJob] = useState<Job>()
 
   const hasValidRole = (role: string) => ["admin", "owner", "recruiter"].includes(role);
+
 
   //check the current URL location for the user, redirect if its recruiter but allow him to view the page.
    const location = useLocation();
@@ -34,12 +38,25 @@ export  function JobEditRouteGuard({ children }: {children: React.ReactNode}) {
       }
 
       try {
-        const [company, roleResult] = await Promise.all([
+        const [company, roleResult, currentJob] = await Promise.all([
           getCompanyById(companyId),
           getUserRole(companyId),
+          getJobById(jobId)
         ]);
 
         const role = roleResult[0]?.role;
+         if (!company.members.includes(user._id)) {
+          toast.error("You are not a member of this company.");
+          navigate("/"); 
+          return;
+        }
+      
+          if (companyId !== currentJob.company) {
+          toast.error("This job is not part of your company.");
+          navigate('/')
+
+          return;
+        }
 
        if (role === "recruiter" && isEditPage) {
           toast.error("Recruiters cannot edit jobs.");
