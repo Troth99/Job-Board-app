ad5be7354cc0ab91cb58f54ad79a49bd3e571f09
Troth99 <trothlol123456@gmail.com>
2026-01-07 04:49:01 +0200

    Integrate form validation in ForgotPassword component
    Added useForm and useValidation hooks to manage form state and validation in the ForgotPassword component. The form now displays validation errors and handles submission using the new hooks.

--- frontend/src/components/auth/forgot-password/Forgot-Password.tsx
+++ frontend/src/components/auth/forgot-password/Forgot-Password.tsx
@@ -1,18 +1,39 @@
+import { useState } from "react"
 import "./ForgotPassword.css"
+import useForm from "../../../hooks/useForm";
+import { useValidation } from "../../validators/useValidation";
+
+const initialFormValue = {
+    email: ''
+}
 
 export function ForgotPassowrd() {
+    const [userEmail, setUserEmail] = useState<string>();
+    const {validateForm} = useValidation()
+
+
+    const resetLinkSendHandler = () => {
+        console.log(values)
+    }
+
+
+  const { values, register, formHandler, errors, setErrors } =
+    useForm(resetLinkSendHandler, initialFormValue, validateForm);
 
     return (
         <div className="forgot-password-page">
 <div className="forgot-password-container">
   <h2>Forgot password</h2>
-  <form className="forgot-password-form">
+  <form className="forgot-password-form" onSubmit={formHandler}>
     <label htmlFor="email">Email:</label>
     <input
       type="email"
       id="email"
-      required
+      placeholder="Peter@yahoo.com"
+      {...register('email')}
+        required
     />
+        {errors && <div className="error-message">{errors.email}</div>}
     <button type="submit">
       Send link for new password
     </button>
