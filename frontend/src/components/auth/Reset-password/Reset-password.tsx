37e4f7775fac72d767f48eff4afd7d92c4b1d46b
Troth99 <trothlol123456@gmail.com>
2026-01-12 04:55:58 +0200

    Add placeholder in resetPasswordHandler function

--- frontend/src/components/auth/Reset-password/Reset-password.tsx
+++ frontend/src/components/auth/Reset-password/Reset-password.tsx
@@ -11,6 +11,7 @@ export default function ResetPassword() {
 
   const resetPasswordHandler = () => {
     //to do reset password handler service 
+    
   };
 
   const { values, register, formHandler, errors } = useForm(
